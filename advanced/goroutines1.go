46f68d30a35174d71cb34456c640b70b1592d69f
Kumar Rohit <thecodebuilder@Rohits-M1-Air.local>
2026-07-07 03:18:47 +0530

    uncommented goroutines code and created fundamentals module

--- advanced/goroutines1.go
+++ advanced/goroutines1.go
@@ -41,179 +41,179 @@ func Goroutine1() {
 		Goroutines enables developers to create multi threaded programs and run these threads concurrently
 	*/
 
-	// // Without goroutines - each start after the last one finishes
-	// run1() // -> run1 started at: 20:12:31
-	// run2() // -> run2 started at: 20:12:33
-	// run3() // -> run3 started at: 20:12:35
+	// Without goroutines - each start after the last one finishes
+	run1() // -> run1 started at: 20:12:31
+	run2() // -> run2 started at: 20:12:33
+	run3() // -> run3 started at: 20:12:35
 
-	// // With goroutines - all start at the same time without waiting for the last one
-	// go run1() // use the go keyword to run a function in a separate thread
-	// go run2()
-	// go run3()
-	// /*
-	// 	->
-	// 	run2 started at: 20:15:13
-	// 	run1 started at: 20:15:13
-	// 	run3 started at: 20:15:13
+	// With goroutines - all start at the same time without waiting for the last one
+	go run1() // use the go keyword to run a function in a separate thread
+	go run2()
+	go run3()
+	/*
+		->
+		run2 started at: 20:15:13
+		run1 started at: 20:15:13
+		run3 started at: 20:15:13
 
-	// 	the order at which the functions are run might not be the same as the order they are called in
-	// */
+		the order at which the functions are run might not be the same as the order they are called in
+	*/
 
-	// time.Sleep(4 * time.Second) // need to wait for the goroutines to finish before the program terminates to print in terminal
+	time.Sleep(4 * time.Second) // need to wait for the goroutines to finish before the program terminates to print in terminal
 
-	// // Channels
+	// Channels
 
-	// /* 
-	// 	They unable devs to return values from goroutines
-	// 	They create blocking operations
-	// 	The program waits for a thread to complete and return the result in a channel before moving forward
-	// */
+	/* 
+		They unable devs to return values from goroutines
+		They create blocking operations
+		The program waits for a thread to complete and return the result in a channel before moving forward
+	*/
 
-	// ch := make(chan int) // create a channel
-	// go addNums(5, 10, ch)
-	// res1 := <- ch // this is a blocking operation
+	ch := make(chan int) // create a channel
+	go addNums(5, 10, ch)
+	res1 := <- ch // this is a blocking operation
 	
-	// fmt.Println(res1)
+	fmt.Println(res1)
 
-	// // Potential deadlock errors when using channels
+	// Potential deadlock errors when using channels
 
-	// ch2 := make(chan int)
-	// go addNumsWithoutReturn(2, 10, ch2) // this function does not return any value in the channel
-	// /*
-	// 	The current program thread (main thread) is waiting for the new thread to return a value which it never does.
-	// 	This causes a deadlock error because the main thread cannot proceed until the new thread returns a value
-	// 	which it never does.
-	// */
-	// res2 := <- ch2 // -> Error (after 2s): fatal error: all goroutines are asleep - deadlock!
-	// fmt.Println(res2)
+	ch2 := make(chan int)
+	go addNumsWithoutReturn(2, 10, ch2) // this function does not return any value in the channel
+	/*
+		The current program thread (main thread) is waiting for the new thread to return a value which it never does.
+		This causes a deadlock error because the main thread cannot proceed until the new thread returns a value
+		which it never does.
+	*/
+	res2 := <- ch2 // -> Error (after 2s): fatal error: all goroutines are asleep - deadlock!
+	fmt.Println(res2)
 
-	// // Multiple goroutines using the same channel - waiting for first value to be returned
+	// Multiple goroutines using the same channel - waiting for first value to be returned
 
-	// ch3 := make(chan int)
-	// go addNums(5, 1, ch3)
-	// go addNums(5, 2, ch3)
-	// go addNums(5, 3, ch3)
-	// go addNums(5, 4, ch3)
-	// /*
-	// 	-> adding 5 and 3
-	// */
+	ch3 := make(chan int)
+	go addNums(5, 1, ch3)
+	go addNums(5, 2, ch3)
+	go addNums(5, 3, ch3)
+	go addNums(5, 4, ch3)
+	/*
+		-> adding 5 and 3
+	*/
 
-	// res2 := <- ch3 // just waits for the first thread to return a value in the channel | the other goroutines will still complete but the program will not wait on this step to catch their returned values
-	// fmt.Println("res2 value:", res2) // -> res2 value: 8
+	res2b := <- ch3 // just waits for the first thread to return a value in the channel | the other goroutines will still complete but the program will not wait on this step to catch their returned values
+	fmt.Println("res2 value:", res2b) // -> res2b value: 8
 
-	// // Multiple goroutines using the same channel - waiting for all values to be returned (in any order)
+	// Multiple goroutines using the same channel - waiting for all values to be returned (in any order)
 
-	// ch4 := make(chan int)
-	// go addNums(5, 1, ch4)
-	// go addNums(5, 2, ch4)
-	// go addNums(5, 3, ch4)
-	// go addNums(5, 4, ch4)
-	// /*
-	// 	->
-	// 	adding 5 and 2
-	// 	adding 5 and 1
-	// 	adding 5 and 3
-	// 	adding 5 and 4
-	// */
+	ch4 := make(chan int)
+	go addNums(5, 1, ch4)
+	go addNums(5, 2, ch4)
+	go addNums(5, 3, ch4)
+	go addNums(5, 4, ch4)
+	/*
+		->
+		adding 5 and 2
+		adding 5 and 1
+		adding 5 and 3
+		adding 5 and 4
+	*/
 
-	// // waits for all the 4 threads to run and finish in any order, then prints the value returned by the last one.
-	// res3 := <- ch4
-	// res3 = <- ch4
-	// res3 = <- ch4
-	// res3 = <- ch4
+	// waits for all the 4 threads to run and finish in any order, then prints the value returned by the last one.
+	res3 := <- ch4
+	res3 = <- ch4
+	res3 = <- ch4
+	res3 = <- ch4
 
-	// fmt.Println("res3 value:", res3) // -> res3 value: 9
+	fmt.Println("res3 value:", res3) // -> res3 value: 9
 
-	// // Multiple goroutines using the same channel - waiting for all values to be returned (in correct order)
+	// Multiple goroutines using the same channel - waiting for all values to be returned (in correct order)
 
-	// ch5 := make(chan int)
+	ch5 := make(chan int)
 
-	// // blocking code that waits for each goroutine to finish before starting the next one - synchronous execution
-	// go addNums(5, 1, ch5)
-	// res4 := <- ch5
-	// go addNums(5, 2, ch5)
-	// res4 = <- ch5
-	// go addNums(5, 3, ch5)
-	// res4 = <- ch5
-	// go addNums(5, 4, ch5)
-	// res4 = <- ch5
-	// /*
-	// 	->
-	// 	adding 5 and 1
-	// 	adding 5 and 2
-	// 	adding 5 and 3
-	// 	adding 5 and 4
+	// blocking code that waits for each goroutine to finish before starting the next one - synchronous execution
+	go addNums(5, 1, ch5)
+	res4 := <- ch5
+	go addNums(5, 2, ch5)
+	res4 = <- ch5
+	go addNums(5, 3, ch5)
+	res4 = <- ch5
+	go addNums(5, 4, ch5)
+	res4 = <- ch5
+	/*
+		->
+		adding 5 and 1
+		adding 5 and 2
+		adding 5 and 3
+		adding 5 and 4
 
-	// 	each printed after 2s
-	// */
+		each printed after 2s
+	*/
 
-	// // prints the value of the last goroutine
-	// fmt.Println("res4 value:", res4) // -> res4 value: 9
+	// prints the value of the last goroutine
+	fmt.Println("res4 value:", res4) // -> res4 value: 9
 
-	// // Multiple go routines using different channels - waiting for all the channels to receive values
+	// Multiple go routines using different channels - waiting for all the channels to receive values
 
-	// ch6 := make(chan int)
-	// ch7 := make(chan int)
+	ch6 := make(chan int)
+	ch7 := make(chan int)
 
-	// go addNums(5, 1, ch6)
-	// go addNums(5, 2, ch7)
-	// /*
-	// 	->
-	// 	adding 5 and 2
-	// 	adding 5 and 1
-	// */
+	go addNums(5, 1, ch6)
+	go addNums(5, 2, ch7)
+	/*
+		->
+		adding 5 and 2
+		adding 5 and 1
+	*/
 
-	// // even though ch7 got the value before ch6, it could not be printed unless both the values are present
-	// // using this syntax forces blocking code to get all the values before processing them
-	// resX := <- ch6
-	// resY := <- ch7
+	// even though ch7 got the value before ch6, it could not be printed unless both the values are present
+	// using this syntax forces blocking code to get all the values before processing them
+	resX := <- ch6
+	resY := <- ch7
 
-	// fmt.Println(resX, resY) // -> 6 7
+	fmt.Println(resX, resY) // -> 6 7
 
-	// // Multiple go routines using different channels - processing the first value as it comes using select
+	// Multiple go routines using different channels - processing the first value as it comes using select
 
-	// ch8 := make(chan int)
-	// ch9 := make(chan int)
+	ch8 := make(chan int)
+	ch9 := make(chan int)
 
-	// go addNums(5, 1, ch8)
-	// go addNums(5, 2, ch9)
+	go addNums(5, 1, ch8)
+	go addNums(5, 2, ch9)
 
-	// /* -> adding 5 and 2 */
+	/* -> adding 5 and 2 */
 
-	// select {
-	// case res5 := <- ch8:
-	// 	fmt.Println("res5 value:", res5)
-	// case res6 := <- ch9:
-	// 	fmt.Println("res6 value:", res6)
-	// }
-	// /* -> res6 value: 7 */
+	select {
+	case res5 := <- ch8:
+		fmt.Println("res5 value:", res5)
+	case res6 := <- ch9:
+		fmt.Println("res6 value:", res6)
+	}
+	/* -> res6 value: 7 */
 
-	// // Multiple go routines using different channels - processing all the values one by one as they come
+	// Multiple go routines using different channels - processing all the values one by one as they come
 
-	// ch10 := make(chan int)
-	// ch11 := make(chan int)
+	ch10 := make(chan int)
+	ch11 := make(chan int)
 
-	// go addNums(5, 1, ch10)
-	// go addNums(5, 2, ch11)
+	go addNums(5, 1, ch10)
+	go addNums(5, 2, ch11)
 
-	// // The loop will make the select statement run twice and wait to receive a value in the channels
-	// for i := 0; i < 2; i++ {
-	// 	select {
-	// 	case res7 := <- ch10:
-	// 		fmt.Println("res7 value:", res7)
-	// 	case res8 := <- ch11:
-	// 		fmt.Println("res8 value:", res8)
-	// 	}
-	// }
-	// /* 
-	// 	-> 
-	// 	adding 5 and 2
-	// 	res8 value: 7
-	// 	adding 5 and 1
-	// 	res7 value: 6
-	// */
+	// The loop will make the select statement run twice and wait to receive a value in the channels
+	for i := 0; i < 2; i++ {
+		select {
+		case res7 := <- ch10:
+			fmt.Println("res7 value:", res7)
+		case res8 := <- ch11:
+			fmt.Println("res8 value:", res8)
+		}
+	}
+	/* 
+		-> 
+		adding 5 and 2
+		res8 value: 7
+		adding 5 and 1
+		res7 value: 6
+	*/
 
-	// fmt.Println("Done")
+	fmt.Println("Done")
 
 	Goroutine2()
 }
--- advanced/goroutines2.go
+++ advanced/goroutines2.go
@@ -35,109 +35,109 @@ func sendThreeValues(ch chan int){
 }
 
 func Goroutine2() {
-	// // Using unidirectional channels to send and receive values
+	// Using unidirectional channels to send and receive values
 
-	// ch := make(chan int) // bidirectional channel
+	ch := make(chan int) // bidirectional channel
 
-	// /*
-	// 	we can explicitly type cast the bidirectional channel to only send or receive and then pass to the corresponding functions as below:
-	// 	sendCh := (chan<- int)(ch)      
-	// 	receiveCh := (<-chan int)(ch) 
+	/*
+		we can explicitly type cast the bidirectional channel to only send or receive and then pass to the corresponding functions as below:
+		sendCh := (chan<- int)(ch)      
+		receiveCh := (<-chan int)(ch) 
 
-	// 	However, this is not required in practice as the Go compiler can perform implicit conversion at compile time.
-	// 	So, passing a bidirectional channel to a function accepting unidirectional channel works and is the intended usage.
-	// */
+		However, this is not required in practice as the Go compiler can perform implicit conversion at compile time.
+		So, passing a bidirectional channel to a function accepting unidirectional channel works and is the intended usage.
+	*/
 
-	// go send(ch)
-	// go receive(ch)
+	go send(ch)
+	go receive(ch)
 
 	time.Sleep(2 * time.Second) // needed to stop the main thread from terminating before the goroutines finish
 
-	// // using buffered channels to send values without blocking
+	// using buffered channels to send values without blocking
 
-	// /*
-	// 	The below non-buffer channel example causes the deadlock error, because it is sending a value in the channel, then
-	// 	waits fot it to be received and never runs the next line. 
+	/*
+		The below non-buffer channel example causes the deadlock error, because it is sending a value in the channel, then
+		waits fot it to be received and never runs the next line. 
 
-	// 	ch1 := make(chan int)
-	// 	ch1 <- 10
-	// 	fmt.Println("this never runs")
-	// 	val0 := <- ch1
+		ch1 := make(chan int)
+		ch1 <- 10
+		fmt.Println("this never runs")
+		val0 := <- ch1
 
-	// 	However, using a buffered channel here can solve this problem as the program does not wait for the value to be received,
-	// 	instead the value is passed to a buffer and received later.
-	// */
-	// ch1 := make(chan int, 1) // buffered channel that can hold 1 value
-	// ch1 <- 10
-	// fmt.Println("this runs")
-	// val0 := <- ch1
-	// fmt.Println("value received from buffer:", val0)
+		However, using a buffered channel here can solve this problem as the program does not wait for the value to be received,
+		instead the value is passed to a buffer and received later.
+	*/
+	ch1 := make(chan int, 1) // buffered channel that can hold 1 value
+	ch1 <- 10
+	fmt.Println("this runs")
+	val0 := <- ch1
+	fmt.Println("value received from buffer:", val0)
 
 
-	// // Sending less or equal values than the buffer size - never blocks
+	// Sending less or equal values than the buffer size - never blocks
 
-	// ch2a := make(chan int, 3) // buffered channel that can hold 3 values
-	// ch2a <- 10
-	// ch2a <- 20
-	// ch2a <- 30
+	ch2a := make(chan int, 3) // buffered channel that can hold 3 values
+	ch2a <- 10
+	ch2a <- 20
+	ch2a <- 30
 
-	// fmt.Println("even though values from ch2a are never received, this will not block")
+	fmt.Println("even though values from ch2a are never received, this will not block")
 
-	// ch2 := make(chan int, 3)
-	// go sendThreeValues(ch2)
+	ch2 := make(chan int, 3)
+	go sendThreeValues(ch2)
 
-	// val1 := <- ch2
-	// val2 := <- ch2
-	// val3 := <- ch2
-	// // Since the channel can only send 3 values, the below line causes a deadlock error as it is waiting to receive a value that is never sent
-	// // val4 := <- ch2 // -> Error: fatal error: all goroutines are asleep - deadlock!
-	// fmt.Println(val1, val2, val3)
+	val1 := <- ch2
+	val2 := <- ch2
+	val3 := <- ch2
+	// Since the channel can only send 3 values, the below line causes a deadlock error as it is waiting to receive a value that is never sent
+	// val4 := <- ch2 // -> Error: fatal error: all goroutines are asleep - deadlock!
+	fmt.Println(val1, val2, val3)
 
-	// // Sending more values than the buffer size - can cause blocking
+	// Sending more values than the buffer size - can cause blocking
 
-	// /* 
-	// 	The below two examples does the same thing: send 3 values in a channel and receive only 2.
-	// 	However, the first example does not cause a deadlock issue, but the second one does. 
-	// 	This is because sending more values in a channel than being received is a blocking operation.
-	// 	However, the difference is that the first example is blocking a separate thread created via goroutine,
-	// 	whereas, the second example is blocking the main thread which is throwing the deadlock error.
-	// */
+	/* 
+		The below two examples does the same thing: send 3 values in a channel and receive only 2.
+		However, the first example does not cause a deadlock issue, but the second one does. 
+		This is because sending more values in a channel than being received is a blocking operation.
+		However, the difference is that the first example is blocking a separate thread created via goroutine,
+		whereas, the second example is blocking the main thread which is throwing the deadlock error.
+	*/
 
-	// // first example - blocking goroutine thread
-	// ch3 := make(chan int, 2)
-	// go sendThreeValues(ch3)
+	// first example - blocking goroutine thread
+	ch3 := make(chan int, 2)
+	go sendThreeValues(ch3)
 
-	// val4 := <- ch3
-	// val5 := <- ch3
-	// fmt.Println(val4, val5) // this works and values are printed.
+	val4 := <- ch3
+	val5 := <- ch3
+	fmt.Println(val4, val5) // this works and values are printed.
 
-	// // second example - blocking main thread
-	// // ch4 := make(chan int, 2)
-	// // ch4 <- 10
-	// // ch4 <- 20
-	// // ch4 <- 30
-	
-	// // val6 := <- ch4
-	// // val7 := <- ch4
-
-	// // fmt.Println(val6, val7) // this never runs as thread gets blocked waiting for value to be received
-
-	// /*
-	// 	This can be handled by making space in the buffer for the extra value by receiving some values
-	// */
-
+	// second example - blocking main thread
 	// ch4 := make(chan int, 2)
 	// ch4 <- 10
 	// ch4 <- 20
-
-	// val6 := <- ch4 // clearing out space for a third value by receiving the first value
-	// fmt.Println("val6:", val6)
-
 	// ch4 <- 30
 	
+	// val6 := <- ch4
 	// val7 := <- ch4
-	// val8 := <- ch4
-	// fmt.Println("val7 and val8:", val7, val8)
+
+	// fmt.Println(val6, val7) // this never runs as thread gets blocked waiting for value to be received
+
+	/*
+		This can be handled by making space in the buffer for the extra value by receiving some values
+	*/
+
+	ch4 := make(chan int, 2)
+	ch4 <- 10
+	ch4 <- 20
+
+	val6 := <- ch4 // clearing out space for a third value by receiving the first value
+	fmt.Println("val6:", val6)
+
+	ch4 <- 30
+	
+	val7 := <- ch4
+	val8 := <- ch4
+	fmt.Println("val7 and val8:", val7, val8)
 	
 	Goroutine3()
 } 
